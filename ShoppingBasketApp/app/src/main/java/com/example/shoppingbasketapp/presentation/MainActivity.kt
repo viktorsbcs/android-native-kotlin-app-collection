70b9dd41b351f352c4a689b2424ae55d88ef10bf
viktorsbcs <electricjackiebeats@gmail.com>
2023-04-20 03:14:28 +0300

    adding MainViewModel to MainActivity.kt

--- ShoppingBasketApp/app/src/main/java/com/example/shoppingbasketapp/presentation/MainActivity.kt
+++ ShoppingBasketApp/app/src/main/java/com/example/shoppingbasketapp/presentation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.shoppingbasketapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppingbasketapp.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel : MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java )
+        viewModel.shopList.observe(this) {
+            Log.d("Hello from MainActivity", it.toString())
+        }
+
+        viewModel.getShopList()
     }
 }
